92f84453fd2b698d591d46eabd0758f609bf4278
glenwojnar1.0@gmail.com <glenwojnar@yahoo.com>
2026-09-01 00:00:21 -0400

    added solutiosn

--- IntroductionToProgamming/Part1/003_row_row_row_your_boat/row_row_row_your_boat.py
+++ IntroductionToProgamming/Part1/003_row_row_row_your_boat/row_row_row_your_boat.py
@@ -5,3 +5,7 @@
 # Gently down the stream.
 # Merrily, merrily, merrily, merrily,
 # Life is but a dream.
+print("Row, row, row your boat,")
+print("Gently down the stream.")
+print("Merrily, merrily, merrily, merrily,")
+print("Life is but a dream.")

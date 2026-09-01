ad57dc36f872be5d1c52f95c1708a68f01ed79b5
glenwojnar1.0@gmail.com <glenwojnar@yahoo.com>
2026-08-31 23:56:20 -0400

    Built test

---
+++ IntroductionToProgamming/Part1/003_row_row_row_your_boat/test_row_row_row_your_boat.py
@@ -0,0 +1,11 @@
+def test_emoticon(capsys):
+    import row_row_row_your_boat
+
+    captured = capsys.readouterr()
+
+    assert captured.out.strip().splitlines() == [
+        "Row, row, row, your boat,",
+        "Gently down your stream.",
+        "Merrily, merrily, merrily, merrily,",
+        "Life is but a dream.",
+    ]

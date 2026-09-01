0f7ce1f657a8ff7d7b7edbfc9dc890e269e8da0f
glenwojnar1.0@gmail.com <glenwojnar@yahoo.com>
2026-08-31 23:34:00 -0400

    Built seven brothers

---
+++ IntroductionToProgamming/Part1/002_seven_brothers/seven_brothers.py
@@ -0,0 +1,9 @@
+# This program is supposed to print out the names of the brothers in alphabetical order, but it's not working quite right yet.
+# Please fix the program so that the names are printed in the correct order.
+print("Simeoni")
+print("Juhani")
+print("Eero")
+print("Lauri")
+print("Aapo")
+print("Tuomas")
+print("Timo")

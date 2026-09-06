fb87f6429ac1629d8d5a95128a7b7e885a3e166e
glenwojnar1.0@gmail.com <glenwojnar@yahoo.com>
2026-09-05 22:58:01 -0400

    Built test

--- IntroductionToProgamming/Part1/006_name_twice/test_name_twice.py
+++ IntroductionToProgamming/Part1/006_name_twice/test_name_twice.py
@@ -1,6 +1,12 @@
-def test_name_twice(capsys):
+def test_name_twice(monkeypatch, capsys):
+    monkeypatch.setattr("builtins.input", lambda _: "Mike O'hearn")
+
     import name_twice
 
     captured = capsys.readouterr()
 
-    assert captured.out.strip().splitlines() == ["What is your name"]
+    assert captured.out.strip().splitlines() == [
+        "What is your name",
+        "Mike O'hearn",
+        "Mike O'hearn",
+    ]

da85134554e0bfd9ceffe92db2bb9332a335abaa
glenwojnar1.0@gmail.com <glenwojnar@yahoo.com>
2026-09-07 22:10:52 -0400

    Built solution

--- IntroductionToProgamming/Part1/008_name_and_address/name_and_address.py
+++ IntroductionToProgamming/Part1/008_name_and_address/name_and_address.py
@@ -9,3 +9,12 @@
 # Steve Sanders
 # 91 Station Road
 # London EC05 6AW
+
+first_name = input("What is your first name? ")
+last_name = input("What is your last name? ")
+address = input("What is your address? ")
+city_and_postal_code = input("What is your city and postal code? ")
+
+print(first_name + " " + last_name)
+print(address)
+print(city_and_postal_code)

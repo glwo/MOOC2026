74ad42917846b5d3167d25530bdb5365b7c3a1ae
glenwojnar1.0@gmail.com <glenwojnar@yahoo.com>
2026-09-07 21:09:56 -0400

    Finished test and init solution file

---
+++ IntroductionToProgamming/Part1/007_name_and_exclamation_marks/test_name_and_exclamation_marks.py
@@ -0,0 +1,8 @@
+def test_name_twice(monkeypatch, capsys):
+    monkeypatch.setattr("builtins.input", lambda _: "Mike O'hearn")
+
+    import name_twice
+
+    captured = capsys.readouterr()
+
+    assert captured.out.strip() == "!Mike O'hearn!Mike O'hearn!"

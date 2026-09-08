ec4aa7bf16419b8dd2285c0d346ede91b70b7a62
glenwojnar1.0@gmail.com <glenwojnar@yahoo.com>
2026-09-07 21:10:26 -0400

    Fix import

--- IntroductionToProgamming/Part1/007_name_and_exclamation_marks/test_name_and_exclamation_marks.py
+++ IntroductionToProgamming/Part1/007_name_and_exclamation_marks/test_name_and_exclamation_marks.py
@@ -1,7 +1,7 @@
 def test_name_twice(monkeypatch, capsys):
     monkeypatch.setattr("builtins.input", lambda _: "Mike O'hearn")
 
-    import name_twice
+    import name_and_exclamation_marks
 
     captured = capsys.readouterr()
 
